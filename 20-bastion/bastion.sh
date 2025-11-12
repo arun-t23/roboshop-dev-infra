b36122211577ef0867880b5c3e7178fd57239c15
Arun T <arunt98>
2025-11-11 22:06:45 -0500

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
+
+### in case want to reduce the volume
+# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+
+
 sudo yum install -y yum-utils
 
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
