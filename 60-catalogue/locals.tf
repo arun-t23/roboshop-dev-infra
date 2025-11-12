27ad452cc100c29143faef0af209c6265a16aa46
Arun T <arunt98>
2025-11-11 21:51:13 -0500

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,9 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
   catalogue_sg_id =   data.aws_ssm_parameter.catalogue_sg_id.value
+  vpc_id  = data.aws_ssm_parameter.vpc_id.value
   common_name_suffix  = "${var.project_name}-${var.environment}"
    common_tags   =   {
     Project     =   var.project_name
