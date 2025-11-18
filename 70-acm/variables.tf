29d51e5b21bbdc86510d18144d4e533153e3207e
Arun T <arunt98>
2025-11-17 19:06:07 -0500

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "cloudquestdev.com"
+}
+
+variable "zone_id" {
+    default = "Z101103636O9J5N20O0OE"
+}
