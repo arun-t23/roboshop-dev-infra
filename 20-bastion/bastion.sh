803a777d60455c2bce9e3d86e018764c029ee49b
Arun T <arunt98>
2025-11-17 20:08:47 -0500

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-sudo growpart /dev/nvme0n1 4
-sudo lvextend -L +30G /dev/mapper/RootVG-homeVol
-sudo xfs_growfs /home
-
-### in case want to reduce the volume
-# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
-
+# growing the /home volume for terraform purpose
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
 
 sudo yum install -y yum-utils
-
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-
 sudo yum -y install terraform
+
+# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
+
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/arun-t23/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
+
